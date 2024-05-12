77e4e6b1392e396abc50585e481b9d983b69167f
SubhamMohanta <subhammohanta99@gmail.com>
2024-05-12 22:02:16 +0530

    Done

--- morehooks/src/App.jsx
+++ morehooks/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import UseReducer from './Concepts/UseReducer'
+// import UseReducer from './Concepts/UseReducer'
 import ToDoApp from './Components/ToDoApp'
-import TodoItem from './Components/ToDoItem'
+// import TodoItem from './Components/ToDoItem'
 
 function App() {
   const [count, setCount] = useState(0)
